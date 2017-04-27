1c422a0628ea09b7a0cb89d6fa9e79ee1a262311
Jeff Katz <jeffreyerickatz@gmail.com>
2017-04-27 10:07:08 -0400

    remove extra bracket

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 describe('#largestSubarraySum', function() {
 	it('largest subarray sum', function(){
 		let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4]
-		expect(largestSubarraySum([array])).toEqual(16)
+		expect(largestSubarraySum(array)).toEqual(16)
 	})
 });
